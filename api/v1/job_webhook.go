338d0a97bd1a05c28d587a8d0eaee20a515754c8
林嘉健 <jiajian.lin@rootcloud.com>
2023-07-20 17:47:33 +0800

    dev

--- api/v1/job_webhook.go
+++ api/v1/job_webhook.go
@@ -2,22 +2,24 @@ package v1
 
 import (
 	"context"
-	batchv1 "k8s.io/api/batch/v1"
+	"errors"
+	batchV1 "k8s.io/api/batch/v1"
+	coreV1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/json"
 	"net/http"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+	"strings"
 )
 
-// log is for logging in this package.
-var joblog = logf.Log.WithName("job-mutator")
-
-// TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-
-//+kubebuilder:webhook:path=/mutate-batch-v1-job,mutating=true,failurePolicy=ignore,sideEffects=None,groups=batch,resources=jobs,verbs=create;update,versions=v1,name=mjob.kb.io,admissionReviewVersions=v1
+//+kubebuilder:webhook:path=/mutate-batch-coreV1-job,mutating=true,failurePolicy=ignore,sideEffects=None,groups=batch,resources=jobs,verbs=create;update,versions=coreV1,name=mjob.kb.io,admissionReviewVersions=coreV1
 //+kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;watch;create;update;patch;delete
+
+// log is for logging in this package.
+var logger = logf.Log.WithName("job-mutator")
 
 type JobMutate struct {
 	Client  client.Client
@@ -32,36 +34,173 @@ func NewJobMutate(client client.Client) admission.Handler {
 	}
 }
 
-func (v *JobMutate) Handle(ctx context.Context, req admission.Request) admission.Response {
+const (
+	AnnotationKey = "job-mutator.sre.rootcloud.info/comparison-content"
+)
+
+type ComparisonType string
+
+var (
+	TypeComparisonImage   ComparisonType = "image"
+	TypeComparisonEnv     ComparisonType = "env"
+	TypeComparisonEnvFrom ComparisonType = "envFrom"
+)
+
+func (_ *JobMutate) parseAnnotation(job *batchV1.Job) []ComparisonType {
+	comparisonTypes := make([]ComparisonType, 0)
+
+	if job.Annotations == nil {
+		return comparisonTypes
+	}
+
+	keys, ok := job.Annotations[AnnotationKey]
+	if !ok {
+		return comparisonTypes
+	}
+
+	m := map[ComparisonType]struct{}{
+		TypeComparisonImage:   {},
+		TypeComparisonEnv:     {},
+		TypeComparisonEnvFrom: {},
+	}
+
+	for _, key := range strings.Split(keys, ",") {
+		k := ComparisonType(key)
+		if _, ok := m[k]; ok {
+			comparisonTypes = append(comparisonTypes, k)
+		}
+	}
+
+	return comparisonTypes
+}
+
+func (jm *JobMutate) GetJob(ctx context.Context, name, namespace string) (*batchV1.Job, error) {
+	obj := &batchV1.Job{}
+	if namespace == "" {
+		namespace = coreV1.NamespaceDefault
+	}
+	if name == "" {
+		return obj, errors.New("resource name is required")
+	}
+	objKey := types.NamespacedName{Namespace: namespace, Name: name}
+	err := jm.Client.Get(ctx, objKey, obj)
+	return obj, err
+}
+
+func (jm *JobMutate) Handle(ctx context.Context, req admission.Request) admission.Response {
 	// TODO
-	job := &batchv1.Job{}
-	err := v.decoder.Decode(req, job)
+	newJob := new(batchV1.Job)
+	err := jm.decoder.Decode(req, newJob)
 	if err != nil {
 		return admission.Errored(http.StatusBadRequest, err)
 	}
 
-	if job.Labels == nil {
-		job.Labels = make(map[string]string)
+	oldJob, err := jm.GetJob(ctx, newJob.Name, newJob.Namespace)
+	if err != nil {
+		return admission.Allowed("oldJob not found, skipping.")
 	}
 
-	if _, ok := job.Labels["testa"]; ok {
-		delete(job.Labels, "testa")
-	}
-	//job.Labels["testa"] = "testbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
+	ComparisonTypes := jm.parseAnnotation(newJob)
 
-	resp, err := json.Marshal(job)
+	if len(ComparisonTypes) == 0 {
+		return admission.Allowed("noting to compare, skipping.")
+	}
+
+	isSame := jm.CompareTemplate(newJob.Spec.Template.Spec, oldJob.Spec.Template.Spec, ComparisonTypes)
+	if isSame {
+		return admission.Allowed("job.spec.template.spec is the same, skipping.")
+	}
+
+	newJob.Spec = oldJob.Spec
+	logger.Info("replace new job.spec")
+
+	resp, err := json.Marshal(newJob)
 	if err != nil {
 		return admission.Errored(http.StatusInternalServerError, err)
-	}
-
-	for k, v := range job.GetLabels() {
-		joblog.Info("labeled", k, v)
 	}
 
 	return admission.PatchResponseFromRaw(req.Object.Raw, resp)
 }
 
-//func (v *JobMutate) InjectDecoder(d *admission.Decoder) error {
-//	v.decoder = d
-//	return nil
-//}
+func (_ *JobMutate) CompareContainerImage(left, right coreV1.Container) (isSame bool) {
+	return left.Image == right.Image
+}
+
+func (_ *JobMutate) CompareContainerEnv(left, right coreV1.Container) (isSame bool) {
+	compareFunc := func(left, right coreV1.EnvVar) bool {
+		return left.String() == right.String()
+
+	}
+	if left.Env == nil && right.Env == nil {
+		return true
+	}
+	if len(left.Env) != len(right.Env) {
+		return false
+	}
+
+	for i := range left.Env {
+		if !compareFunc(left.Env[i], right.Env[i]) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (_ *JobMutate) CompareContainerEnvFrom(left, right coreV1.Container) (isSame bool) {
+	compareFunc := func(left, right coreV1.EnvFromSource) bool {
+		return left.String() == right.String()
+	}
+
+	if left.EnvFrom == nil && right.EnvFrom == nil {
+		return true
+	}
+	if len(left.EnvFrom) != len(right.EnvFrom) {
+		return false
+	}
+
+	for i := range left.EnvFrom {
+		if !compareFunc(left.EnvFrom[i], right.EnvFrom[i]) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (jm *JobMutate) CompareTemplate(left, right coreV1.PodSpec, comparisonContent []ComparisonType) (isSame bool) {
+	m := make(map[ComparisonType]struct{})
+	for _, c := range comparisonContent {
+		m[c] = struct{}{}
+	}
+
+	if len(left.Containers) != len(right.Containers) {
+		return false
+	}
+
+	for i := range left.Containers {
+		leftContainer := left.Containers[i]
+		rightContainer := right.Containers[i]
+
+		if _, ok := m[TypeComparisonImage]; ok {
+			if jm.CompareContainerImage(leftContainer, rightContainer) {
+				return false
+			}
+		}
+
+		if _, ok := m[TypeComparisonEnv]; ok {
+			if jm.CompareContainerEnv(leftContainer, rightContainer) {
+				return false
+			}
+		}
+
+		if _, ok := m[TypeComparisonEnvFrom]; ok {
+			if jm.CompareContainerEnvFrom(leftContainer, rightContainer) {
+				return false
+			}
+		}
+	}
+
+	// finally
+	return true
+}
--- api/v1/job_webhook_test.go
+++ api/v1/job_webhook_test.go
@@ -1,12 +1,51 @@
 package v1
 
 import (
+	"context"
+	"flag"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"path/filepath"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 	"testing"
 )
 
-func TestHandler(t *testing.T) {
-	//jm := &JobMutate{
-	//	Client:  nil,
-	//	decoder: *admission.Decoder,
-	//}
+func newClient() client.Client {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	c, err := client.New(config, client.Options{})
+	if err != nil {
+		panic(err)
+	}
+
+	return c
+
+}
+
+func TestGetJob(t *testing.T) {
+	c := newClient()
+
+	jm := JobMutate{
+		Client:  c,
+		decoder: admission.NewDecoder(runtime.NewScheme()),
+	}
+	job, err := jm.GetJob(context.TODO(), "a", "")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	t.Log(job.String())
+
 }
