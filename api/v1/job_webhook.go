e8ca38cb8da903335a4428717daefcb5cc486ac9
林嘉健 <jiajian.lin@rootcloud.com>
2023-07-20 11:29:03 +0800

    dev

--- api/v1/job_webhook.go
+++ api/v1/job_webhook.go
@@ -2,7 +2,6 @@ package v1
 
 import (
 	"context"
-	"fmt"
 	batchv1 "k8s.io/api/batch/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/json"
@@ -20,21 +19,10 @@ var joblog = logf.Log.WithName("job-mutator")
 //+kubebuilder:webhook:path=/mutate-batch-v1-job,mutating=true,failurePolicy=ignore,sideEffects=None,groups=batch,resources=jobs,verbs=create;update,versions=v1,name=mjob.kb.io,admissionReviewVersions=v1
 //+kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;watch;create;update;patch;delete
 
-//var _ webhook.Defaulter = &Job{}
-//
-//// Default implements webhook.Defaulter so a webhook will be registered for the type
-//func (r *Job) Default() {
-//	joblog.Info("default", "name", r.Name)
-//
-//	// TODO(user): fill in your defaulting logic.
-//}
-
 type JobMutate struct {
 	Client  client.Client
 	decoder *admission.Decoder
 }
-
-var _ admission.Handler = &JobMutate{}
 
 func NewJobMutate(client client.Client) admission.Handler {
 	decoder := admission.NewDecoder(runtime.NewScheme())
@@ -47,16 +35,19 @@ func NewJobMutate(client client.Client) admission.Handler {
 func (v *JobMutate) Handle(ctx context.Context, req admission.Request) admission.Response {
 	// TODO
 	job := &batchv1.Job{}
-	joblog.Info(fmt.Sprintf("%#v", v))
-	joblog.Info(fmt.Sprintf("%#v", v.decoder))
-	joblog.Info(fmt.Sprintf("%#v", req))
-	//joblog.Info(fmt.Sprintf("%#v", v.decoder.Decode(req, nil)))
-
 	err := v.decoder.Decode(req, job)
 	if err != nil {
 		return admission.Errored(http.StatusBadRequest, err)
 	}
-	job.Labels["testaaaaa"] = "testbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
+
+	if job.Labels == nil {
+		job.Labels = make(map[string]string)
+	}
+
+	if _, ok := job.Labels["testa"]; ok {
+		delete(job.Labels, "testa")
+	}
+	//job.Labels["testa"] = "testbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
 
 	resp, err := json.Marshal(job)
 	if err != nil {
@@ -66,7 +57,7 @@ func (v *JobMutate) Handle(ctx context.Context, req admission.Request) admission
 	for k, v := range job.GetLabels() {
 		joblog.Info("labeled", k, v)
 	}
-	//joblog.Info("labeled", job.GetLabels())
+
 	return admission.PatchResponseFromRaw(req.Object.Raw, resp)
 }
 
